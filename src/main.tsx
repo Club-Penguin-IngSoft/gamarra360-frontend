fe967b575f3e8792a99cb4e953c08506f614d0e4
Gianca <giancarlovilchezcanaza1@gmail.com>
2026-05-28 01:17:07 -0500

    frontDelLogin

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
 import AppRouter from './router';
 import { StoreProvider } from './store';
+
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <StoreProvider>
-      <AppRouter />
-    </StoreProvider>
-  </BrowserRouter>,
+  <GoogleOAuthProvider clientId="1054501319788-fd9c67d26p5c2ne8qnvj5ltjarp2s5c3.apps.googleusercontent.com">
+    <BrowserRouter>
+      <StoreProvider>
+        <AppRouter />
+      </StoreProvider>
+    </BrowserRouter>
+  </GoogleOAuthProvider>
 );
